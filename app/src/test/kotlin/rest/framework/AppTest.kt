7351d0f44a4be0951298b34ef971d9e41d898d49
Kristy Welsh <kriswelsh@gmail.com>
2022-08-27 18:06:32 +0100

    address compilation warnings

--- app/src/test/kotlin/rest/framework/AppTest.kt
+++ app/src/test/kotlin/rest/framework/AppTest.kt
@@ -9,28 +9,28 @@ class Character(name: String) {
 
 class EndpointTest {
 
-	@Test fun endpoint_responds_to_GET_request() {
-		val endpoint = Endpoint<Character>("character")
+	lateinit var endpoint: Endpoint<Character>
 
+	@BeforeTest fun setup() {
+		endpoint = Endpoint("character")
+	}
+
+	@Test fun endpoint_responds_to_GET_request() {
 		endpoint.get({ Response.response200(Character("steve")) })
 
 		assertEquals(endpoint.recieveGet()?.data?.name, "steve")
 	}
 
 	@Test fun endpoint_responds_to_POST_request() {
-		val endpoint = Endpoint<Character>("character")
-
 		endpoint.post({ Response.response201("arbitrary string") })
 
 		assertEquals(endpoint.recievePost()?.data, "arbitrary string")
 	}
 
 	@Test fun endpoint_responds_to_DELETE_request() {
-		val endpoint = Endpoint<Character>("character")
-
 		endpoint.delete({ Response.response204() })
 
-		assertEquals(endpoint.recieveDelete()?.body, "")
+		assertEquals(endpoint.recieveDelete().body, "")
 	}
 }
 
